78a3cd74bdd6a6fac6d2dce010043a7a9708e851
zsofi <zsofiath@gmail.com>
2022-02-12 18:07:27 +0100

    :sparkles: :poop: error handling!!! catches not the expected error is the firebase url is wrong

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,51 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-import { useEffect, useState } from 'react';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setmeals] = useState([]);
-const [isLoading, setisLoading] = useState(true);
+  const [isLoading, setisLoading] = useState(true);
+  const [httpError, sethttpError] = useState();
 
   useEffect(() => {
     const fetchMeal = async () => {
-      const response = await fetch('https://react-practice-333ee-default-rtdb.firebaseio.com/meals.json');
+      const response = await fetch(
+        "https://react-practice-333ee-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       const loadedMeals = [];
 
       for (const key in responseData) {
         loadedMeals.push({
-          id: key, 
-          name:responseData[key].name,
-          description:responseData[key].description,
-          price:responseData[key].price
-        })
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
       }
-      setmeals(loadedMeals)
-      setisLoading(false)
-    }
-    fetchMeal();
-  }, [])
-  
-  if(isLoading) {
-    return (
-    <section className={classes.MealsLoading}>Loading</section>
-    )
+      setmeals(loadedMeals);
+      setisLoading(false);
+    };
+
+
+    fetchMeal().catch((error) => {
+      setisLoading(false);
+      sethttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>Loading</section>;
   }
+
+  if (httpError) {
+    return <section className={classes.MealsLoading}>{httpError}</section>;
+  } 
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
