736019bb2fb7934c7a447ace5ee09cd85e4cae92
zsofi <zsofiath@gmail.com>
2022-02-12 20:42:02 +0100

    :sparkles: upgrade form validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,51 +1,64 @@
 import { useRef, useState } from "react";
+import useValiation from "../UI/utilities/useValidation";
 import classes from "./Checkout.module.css";
+const isNotEmpty = (value) => value.trim() !== "";
 const isEmpty = (value) => value.trim() === "";
 const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
-  const [formInputValidity, setformInputValidity] = useState({
-    name: true,
-    street: true,
-    city: true,
-    postal: true,
-  });
 
-  const nameInputRef = useRef();
-  const streetInputRef = useRef();
-  const postalInputRef = useRef();
-  const cityInputRef = useRef();
+  const {
+    value: namevalue,
+    isValid: nameIsValid,
+    hasError: namehasError,
+    valueBlurHandler: namevalueBlurHandler,
+    valueChangeHandler: namevalueChangeHandler,
+  } = useValiation(isNotEmpty);
+
+  const {
+    value: streetvalue,
+    isValid: streetIsValid,
+    hasError: streethasError,
+    valueBlurHandler: streetvalueBlurHandler,
+    valueChangeHandler: streetvalueChangeHandler,
+  } = useValiation(isNotEmpty);
+
+  const {
+    value: cityvalue,
+    isValid: cityIsValid,
+    hasError: cityhasError,
+    valueBlurHandler: cityvalueBlurHandler,
+    valueChangeHandler: cityvalueChangeHandler,
+  } = useValiation(isNotEmpty);
+
+  const {
+    value: postalvalue,
+    isValid: postalIsValid,
+    hasError: postalhasError,
+    valueBlurHandler: postalvalueBlurHandler,
+    valueChangeHandler: postalvalueChangeHandler,
+  } = useValiation(isFiveChars);
 
   const confirmHandler = (event) => {
     event.preventDefault();
-    const enteredName = nameInputRef.current.value;
-    const enteredStreet = streetInputRef.current.value;
-    const enteredPostal = postalInputRef.current.value;
-    const enteredCity = cityInputRef.current.value;
 
-    const enteredNameIsValid = !isEmpty(enteredName);
-    const enteredStreetIsValid = !isEmpty(enteredStreet);
-    const enteredCityIsValid = !isEmpty(enteredCity);
-    const enteredPostalIsValid = isFiveChars(enteredPostal);
+    namevalueBlurHandler();
+    streetvalueBlurHandler();
+    cityvalueBlurHandler();
+    postalvalueBlurHandler();
 
-    setformInputValidity({
-      name: enteredNameIsValid,
-      street: enteredStreetIsValid,
-      city: enteredCityIsValid,
-      postal: enteredPostalIsValid,
-    });
     const formIsValid =
-      enteredNameIsValid &&
-      enteredStreetIsValid &&
-      enteredCityIsValid &&
-      enteredPostalIsValid;
+      nameIsValid &&
+      streetIsValid &&
+      cityIsValid &&
+      postalIsValid;
 
     if (formIsValid) {
       props.onConfirm({
-        name: enteredName,
-        street: enteredStreet,
-        city: enteredCity,
-        postal: enteredPostal,
+        name: namevalue,
+        street: streetvalue,
+        city: cityvalue,
+        postal: postalvalue,
       });
     }
   };
@@ -54,23 +67,51 @@ const Checkout = (props) => {
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" ref={nameInputRef} id="name" />
-        {!formInputValidity.name && <p>Invalid</p>}
+        <input
+          autoComplete="false"
+          type="text"
+          id="name"
+          value={namevalue}
+          onBlur={namevalueBlurHandler}
+          onChange={namevalueChangeHandler}
+        />
+        {namehasError && <p>Invalid</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" ref={streetInputRef} id="street" />
-        {!formInputValidity.street && <p>Invalid</p>}
+        <input
+          autoComplete="false"
+          type="text"
+          value={streetvalue}
+          onBlur={streetvalueBlurHandler}
+          onChange={streetvalueChangeHandler}
+          id="street"
+        />
+        {streethasError && <p>Invalid</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" ref={postalInputRef} id="postal" />
-        {!formInputValidity.city && <p>Invalid</p>}
+        <input
+          autoComplete="false"
+          type="text"
+          value={postalvalue}
+          onBlur={postalvalueBlurHandler}
+          onChange={postalvalueChangeHandler}
+          id="postal"
+        />
+        {postalhasError && <p>Invalid</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" ref={cityInputRef} id="city" />
-        {!formInputValidity.postal && <p>Invalid</p>}
+        <input
+          autoComplete="false"
+          type="text"
+          value={cityvalue}
+          onBlur={cityvalueBlurHandler}
+          onChange={cityvalueChangeHandler}
+          id="city"
+        />
+        {cityhasError && <p>Invalid</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
---
+++ src/components/UI/utilities/useValidation.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const useValiation = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const enteredValueIsValid = validateValue(enteredValue);
+  const hasError = !enteredValueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const valueBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setIsTouched(false);
+    setEnteredValue('');
+  }
+
+  return {
+    value: enteredValue,
+    isValid: enteredValueIsValid,
+    hasError,
+    reset,
+    valueChangeHandler,
+    valueBlurHandler,
+  };
+};
+
+export default useValiation;
